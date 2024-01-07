f37f501ad8fa9a0e8e8af969ab46395075a6055b
Ryan Koppenhaver <ryan.koppenhaver@gmail.com>
2024-01-07 01:32:54 -0800

    go fmt

--- main.go
+++ main.go
@@ -4,19 +4,19 @@ import (
 	_ "embed"
 	"encoding/json"
 	"encoding/xml"
-  "fmt"
+	"fmt"
 	"io"
-  "log/slog"
+	"log/slog"
 	"net/http"
-  "net/url"
-  "os"
-  "regexp"
+	"net/url"
+	"os"
+	"regexp"
 	"strconv"
 	"time"
 
-  "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 	"github.com/hashicorp/golang-lru/v2/expirable"
-  "github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 const XMLPrefix = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
@@ -28,12 +28,12 @@ const postsUrlTemplate = "https://www.patreon.com/api/posts?fields[post]=title,u
 const searchUrlTemplate = "https://www.patreon.com/api/search?q=%s&page%%5Bsize%%5D=5&json-api-version=1.0&include=[]"
 
 type CampaignAPIResponse struct {
-  Data struct {
-    Attributes struct {
-      Name string
-      URL string
-    }
-  }
+	Data struct {
+		Attributes struct {
+			Name string
+			URL  string
+		}
+	}
 }
 
 type PostsAPIResponse struct {
@@ -46,36 +46,36 @@ type Post struct {
 }
 
 type PostAttributes struct {
-  Content string
-  PublishedAt time.Time `json:"published_at"`
-  TeaserText string `json:"teaser_text"`
-	Title string
-	URL   string
+	Content     string
+	PublishedAt time.Time `json:"published_at"`
+	TeaserText  string    `json:"teaser_text"`
+	Title       string
+	URL         string
 }
 
 type SearchAPIResponse struct {
-  Data []struct {
-    Attributes struct {
-      CreatorName string `json:"creator_name"`
-      CreationName string `json:"creation_name"`
-      URL string
-    }
-    ID string `json:"id"`
-  }
+	Data []struct {
+		Attributes struct {
+			CreatorName  string `json:"creator_name"`
+			CreationName string `json:"creation_name"`
+			URL          string
+		}
+		ID string `json:"id"`
+	}
 }
 
 type FrontendSearchResult struct {
-  Name string `json:"name"`
-  Desc string `json:"desc"`
-  ID string `json:"id"`
-  URL string `json:"url"`
+	Name string `json:"name"`
+	Desc string `json:"desc"`
+	ID   string `json:"id"`
+	URL  string `json:"url"`
 }
 
 type Feed struct {
 	XMLName xml.Name `xml:"feed"`
-  XMLNS   string `xml:"xmlns,attr"`
+	XMLNS   string   `xml:"xmlns,attr"`
 
-  //Author  string `xml:"author"` //required by the spec, but blank is also wrong, so just leave it off for now
+	//Author  string `xml:"author"` //required by the spec, but blank is also wrong, so just leave it off for now
 	ID      string `xml:"id"`
 	Title   string `xml:"title"`
 	Updated string `xml:"updated"`
@@ -84,26 +84,27 @@ type Feed struct {
 	Entries []FeedEntry `xml:"entry"`
 }
 
-type FeedContent struct{
-  Type string `xml:"type,attr"`
-  Content string `xml:",chardata"`
+type FeedContent struct {
+	Type    string `xml:"type,attr"`
+	Content string `xml:",chardata"`
 }
 
-type FeedEntry struct{
-  Title string `xml:"title"`
-  Content FeedContent `xml:"content"`
-  Link Link `xml:"link"`
-  Updated time.Time `xml:"updated"`
+type FeedEntry struct {
+	Title   string      `xml:"title"`
+	Content FeedContent `xml:"content"`
+	Link    Link        `xml:"link"`
+	Updated time.Time   `xml:"updated"`
 }
 
-type Link struct{
-  Rel string `xml:"rel,attr"`
-  Type string `xml:"type,attr"`
-  HRef string `xml:"href,attr"`
+type Link struct {
+	Rel  string `xml:"rel,attr"`
+	Type string `xml:"type,attr"`
+	HRef string `xml:"href,attr"`
 }
 
 //go:embed home.html
 var homeHTML string
+
 //go:embed htm-preact-standalone.mjs
 var htmJS string
 
@@ -112,58 +113,57 @@ var postsCache = expirable.NewLRU[int, *PostsAPIResponse](1000, nil, 15*time.Min
 var searchCache = expirable.NewLRU[string, *SearchAPIResponse](1000, nil, 1*time.Hour)
 
 func main() {
-  
-  var logFormatter gin.LogFormatter
 
-  if gin.Mode() == gin.DebugMode {
-    slog.SetDefault(slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
-      Level: slog.LevelDebug,
-    })))
-  } else {
-    slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, nil)))
-    logFormatter = formatJSONLog
-  }
+	var logFormatter gin.LogFormatter
 
-  slog.Info("application start")
+	if gin.Mode() == gin.DebugMode {
+		slog.SetDefault(slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		})))
+	} else {
+		slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, nil)))
+		logFormatter = formatJSONLog
+	}
 
-  router := gin.New()
-  router.Use(gin.LoggerWithConfig(gin.LoggerConfig{
-    Formatter: logFormatter,
-    SkipPaths: []string{"/favicon.ico"},
-  }))
-  router.Use(gin.Recovery())
-    
-  router.GET("/", handleHome)
-  router.GET("/htm-preact-standalone.mjs", func(c *gin.Context) { 
-    c.Writer.Header().Set("Content-Type", "application/javascript")
-    c.String(200, "%s", htmJS)
-  })
-  router.GET("/feed/:id", handleFeed)
-  router.GET("/search", handleSearch)
+	slog.Info("application start")
 
-  router.SetTrustedProxies(nil)
+	router := gin.New()
+	router.Use(gin.LoggerWithConfig(gin.LoggerConfig{
+		Formatter: logFormatter,
+		SkipPaths: []string{"/favicon.ico"},
+	}))
+	router.Use(gin.Recovery())
 
-  router.Run(":8000")
+	router.GET("/", handleHome)
+	router.GET("/htm-preact-standalone.mjs", func(c *gin.Context) {
+		c.Writer.Header().Set("Content-Type", "application/javascript")
+		c.String(200, "%s", htmJS)
+	})
+	router.GET("/feed/:id", handleFeed)
+	router.GET("/search", handleSearch)
+
+	router.SetTrustedProxies(nil)
+
+	router.Run(":8000")
 }
 
 func formatJSONLog(p gin.LogFormatterParams) string {
-  b, err := json.Marshal(gin.H{
-    "time": p.TimeStamp,
-    "level": "INFO",
-    "method": p.Method,
-    "path": p.Path,
-    "status": p.StatusCode,
-    "duration": p.Latency,
-    "size": p.BodySize,
-    "error": p.ErrorMessage,
-  })
-  if err != nil {
-    slog.Error("failed to marshal request log", "error", err)
-    return fmt.Sprintf("unmarshalable: %+v : %v\n", p, err)
-  }
-  return fmt.Sprintf("%s\n", b)
+	b, err := json.Marshal(gin.H{
+		"time":     p.TimeStamp,
+		"level":    "INFO",
+		"method":   p.Method,
+		"path":     p.Path,
+		"status":   p.StatusCode,
+		"duration": p.Latency,
+		"size":     p.BodySize,
+		"error":    p.ErrorMessage,
+	})
+	if err != nil {
+		slog.Error("failed to marshal request log", "error", err)
+		return fmt.Sprintf("unmarshalable: %+v : %v\n", p, err)
+	}
+	return fmt.Sprintf("%s\n", b)
 }
-
 
 func handleHome(c *gin.Context) {
 	c.Writer.Header().Set("Content-Type", "text/html")
@@ -173,29 +173,28 @@ func handleHome(c *gin.Context) {
 var campaignIDPattern = regexp.MustCompile(`^campaign_(\d+)$`)
 
 func handleSearch(c *gin.Context) {
-  q := c.Query("q")
-  results, err := fetchWithCache(searchUrlTemplate, url.QueryEscape(q), searchCache)
-  if err != nil {
-    fail(c, "search", err)
-    return
-  }
+	q := c.Query("q")
+	results, err := fetchWithCache(searchUrlTemplate, url.QueryEscape(q), searchCache)
+	if err != nil {
+		fail(c, "search", err)
+		return
+	}
 
-  res := make([]FrontendSearchResult, len(results.Data))
-  for idx, item := range results.Data {
-    matches := campaignIDPattern.FindStringSubmatch(item.ID)
-    if matches == nil || len(matches) < 2 {
-      fail(c, "search results", fmt.Errorf("bad id: %s", item.ID))
-    }
+	res := make([]FrontendSearchResult, len(results.Data))
+	for idx, item := range results.Data {
+		matches := campaignIDPattern.FindStringSubmatch(item.ID)
+		if matches == nil || len(matches) < 2 {
+			fail(c, "search results", fmt.Errorf("bad id: %s", item.ID))
+		}
 
-    res[idx].Name = item.Attributes.CreatorName
-    res[idx].Desc = item.Attributes.CreationName
-    res[idx].ID = matches[1]
-    res[idx].URL = item.Attributes.URL
-  }
+		res[idx].Name = item.Attributes.CreatorName
+		res[idx].Desc = item.Attributes.CreationName
+		res[idx].ID = matches[1]
+		res[idx].URL = item.Attributes.URL
+	}
 
-  c.JSON(200, res)
+	c.JSON(200, res)
 }
-
 
 func handleFeed(c *gin.Context) {
 	id, _ := strconv.Atoi(c.Param("id"))
@@ -206,134 +205,133 @@ func handleFeed(c *gin.Context) {
 		return
 	}
 
-  campaign, err := fetchWithCache(campaignUrlTemplate, id, campaignCache)
+	campaign, err := fetchWithCache(campaignUrlTemplate, id, campaignCache)
 	if err != nil {
-    fail(c, "fetch campaign", err)
-    return
-  }
-  
-  posts, err := fetchWithCache(postsUrlTemplate, id, postsCache)
-	if err != nil {
-    fail(c, "fetch posts", err)
+		fail(c, "fetch campaign", err)
 		return
 	}
 
-  entries := make([]FeedEntry, len(posts.Data))
-  for idx, post := range posts.Data {
-    fc := FeedContent{}
-    if post.Attributes.Content != "" {
-      fc.Type = "html"
-      fc.Content = post.Attributes.Content
-    } else {
-      fc.Type = "text"
-      fc.Content = post.Attributes.TeaserText
-    }
+	posts, err := fetchWithCache(postsUrlTemplate, id, postsCache)
+	if err != nil {
+		fail(c, "fetch posts", err)
+		return
+	}
 
-    entries[idx] = FeedEntry{
-      Title: post.Attributes.Title,
-      Content: fc,
-      Link: Link{
-        Rel: "alternate",
-        Type: HTMLType,
-        HRef: post.Attributes.URL,
-      },
-      Updated: post.Attributes.PublishedAt,
-    }    
-  }
+	entries := make([]FeedEntry, len(posts.Data))
+	for idx, post := range posts.Data {
+		fc := FeedContent{}
+		if post.Attributes.Content != "" {
+			fc.Type = "html"
+			fc.Content = post.Attributes.Content
+		} else {
+			fc.Type = "text"
+			fc.Content = post.Attributes.TeaserText
+		}
+
+		entries[idx] = FeedEntry{
+			Title:   post.Attributes.Title,
+			Content: fc,
+			Link: Link{
+				Rel:  "alternate",
+				Type: HTMLType,
+				HRef: post.Attributes.URL,
+			},
+			Updated: post.Attributes.PublishedAt,
+		}
+	}
 
 	feed := Feed{
-    XMLNS: "http://www.w3.org/2005/Atom",
-		ID: fullURL(c.Request).String(),
-    Title: fmt.Sprintf("Patreon: %s", campaign.Data.Attributes.Name),
-    Link: []Link{{
-      Rel: "alternate",
-      Type: HTMLType,
-      HRef: campaign.Data.Attributes.URL,
-    }, {
-      Rel: "self",
-      Type: AtomType,
-      HRef: fullURL(c.Request).String(),
-    }},
-    Updated: time.Now().UTC().Format(time.RFC3339),
+		XMLNS: "http://www.w3.org/2005/Atom",
+		ID:    fullURL(c.Request).String(),
+		Title: fmt.Sprintf("Patreon: %s", campaign.Data.Attributes.Name),
+		Link: []Link{{
+			Rel:  "alternate",
+			Type: HTMLType,
+			HRef: campaign.Data.Attributes.URL,
+		}, {
+			Rel:  "self",
+			Type: AtomType,
+			HRef: fullURL(c.Request).String(),
+		}},
+		Updated: time.Now().UTC().Format(time.RFC3339),
 		Entries: entries,
 	}
 
 	out, err := xml.MarshalIndent(feed, "", "  ")
 	if err != nil {
-    fail(c, "marshal", err)
+		fail(c, "marshal", err)
 		return
 	}
 
-  io.WriteString(c.Writer, XMLPrefix)
+	io.WriteString(c.Writer, XMLPrefix)
 	_, err = c.Writer.Write(out)
-  if err != nil {
-    // this is what gin's Render methods do
-    c.Error(err)
-    c.Abort()
-  }
+	if err != nil {
+		// this is what gin's Render methods do
+		c.Error(err)
+		c.Abort()
+	}
 }
 
 func fullURL(r *http.Request) *url.URL {
-  scheme := "http"
-  if r.TLS != nil {
-    scheme = "https"
-  }
-  base := url.URL{
-    Scheme: scheme,
-    Host: r.Host,
-  }
-  return base.ResolveReference(r.URL)
+	scheme := "http"
+	if r.TLS != nil {
+		scheme = "https"
+	}
+	base := url.URL{
+		Scheme: scheme,
+		Host:   r.Host,
+	}
+	return base.ResolveReference(r.URL)
 }
 
 func fetch(url string) ([]byte, error) {
-  resp, err := http.Get(url)
-  if err !=  nil {
-    return nil, fmt.Errorf("get %s: %w", url, err)
-  }
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("get %s: %w", url, err)
+	}
 
-  defer resp.Body.Close()
-  body, err := io.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
 
-  if err != nil {
-    return nil, fmt.Errorf("get %s: read body: %v", err)
-  }
+	if err != nil {
+		return nil, fmt.Errorf("get %s: read body: %v", err)
+	}
 
-  if resp.StatusCode != 200 {
-    return nil, fmt.Errorf("get %s: status %s: %s", url, resp.Status, string(body))
-  }
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("get %s: status %s: %s", url, resp.Status, string(body))
+	}
 
-  slog.Debug("fetched", "url", url, "res body", string(body))
+	slog.Debug("fetched", "url", url, "res body", string(body))
 
-  return body, nil
+	return body, nil
 }
 
 func fetchWithCache[K comparable, S any](urlTemplate string, key K, cache *expirable.LRU[K, *S]) (*S, error) {
-  s, ok := cache.Get(key)
-  cacheChecks.With(prometheus.Labels{"type" : fmt.Sprintf("%T", *new(S)), "hit": strconv.FormatBool(ok)}).Inc()
-  if ok {
-    slog.Debug("cache hit", "key", key)
-    return s, nil
-  }
+	s, ok := cache.Get(key)
+	cacheChecks.With(prometheus.Labels{"type": fmt.Sprintf("%T", *new(S)), "hit": strconv.FormatBool(ok)}).Inc()
+	if ok {
+		slog.Debug("cache hit", "key", key)
+		return s, nil
+	}
 
-  slog.Debug("cache miss", "key", key)
+	slog.Debug("cache miss", "key", key)
 
-  body, err := fetch(fmt.Sprintf(urlTemplate, key))
-  if err != nil {
-    return nil, err
-  }
+	body, err := fetch(fmt.Sprintf(urlTemplate, key))
+	if err != nil {
+		return nil, err
+	}
 
-  s = new(S)
-  err = json.Unmarshal(body, s)
-  if err != nil {
-    return nil, err
-  }
+	s = new(S)
+	err = json.Unmarshal(body, s)
+	if err != nil {
+		return nil, err
+	}
 
-  cache.Add(key, s)
-  return s, nil
+	cache.Add(key, s)
+	return s, nil
 }
 
 func fail(c *gin.Context, context string, err error) {
-  c.Writer.WriteHeader(500)
-  io.WriteString(c.Writer, fmt.Sprintf("internal error: %s: %v", context, err))
+	c.Writer.WriteHeader(500)
+	io.WriteString(c.Writer, fmt.Sprintf("internal error: %s: %v", context, err))
 }
-
--- metrics.go
+++ metrics.go
@@ -1,31 +1,28 @@
 package main
 
 import (
-        "net/http"
+	"net/http"
 
-        "github.com/prometheus/client_golang/prometheus"
-        "github.com/prometheus/client_golang/prometheus/promauto"
-        "github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 var (
-  cacheChecks = promauto.NewCounterVec(prometheus.CounterOpts{
-    Name: "upstream_api_cache_check",
-    Help: "Number of hits/misses for the app's in-memory cache of upstream API results",
-  }, []string{"type", "hit"})
+	cacheChecks = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "upstream_api_cache_check",
+		Help: "Number of hits/misses for the app's in-memory cache of upstream API results",
+	}, []string{"type", "hit"})
 )
 
-
 func init() {
-  go func() {
-    //mux = http.NewServeMux()
-    s := &http.Server{
-      Addr: ":2112",
-      Handler: promhttp.Handler(),
-    }
-    //mux.Handle("/metrics", promhttp.Handler())
-    s.ListenAndServe()
-  }()
+	go func() {
+		//mux = http.NewServeMux()
+		s := &http.Server{
+			Addr:    ":2112",
+			Handler: promhttp.Handler(),
+		}
+		//mux.Handle("/metrics", promhttp.Handler())
+		s.ListenAndServe()
+	}()
 }
-
-
